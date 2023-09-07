65b5c4dc17b32798379a76331755357bfafc9b69
shadysamara <shady.samara@gmail.com>
2023-09-07 11:07:00 +0300

    ex2

---
+++ bin/ex2/student_data.dart
@@ -0,0 +1,16 @@
+Map studentMap = {
+  "id": 120231122,
+  "name": "Ali",
+  "Grades": {
+    "20221": [
+      {"name": "arabic", "hours": 3, "grade": 90},
+      {"name": "English", "hours": 2, "grade": 95},
+      {"name": "Math", "hours": 3, "grade": 77},
+    ],
+    "20222": [
+      {"name": "Flutter", "hours": 3, "grade": 89},
+      {"name": "Php", "hours": 2, "grade": 69},
+      {"name": "Python", "hours": 3, "grade": 77},
+    ]
+  },
+};
